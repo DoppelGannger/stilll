bdb3e0753bfe94ec7fc061871a60df426cf8bd4c
Drdiz <drdiz@mail.ru>
2023-06-29 14:51:37 +0300

    first commi52443t

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-window.onscroll = function() {//событие скрола
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if(scrolled >= 123) {
-      document.getElementById("nav-sticky").classList.add("fixed-top");
-      } else if (scrolled < 124) {
-          document.getElementById("nav-sticky").classList.remove("fixed-top");
-      }
-}
+// window.onscroll = function() {//событие скрола
+//   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+//   if(scrolled >= 123) {
+//       document.getElementById("nav-sticky").classList.add("fixed-top");
+//       } else if (scrolled < 124) {
+//           document.getElementById("nav-sticky").classList.remove("fixed-top");
+//       }
+// }
